6466b837945f511cf1e0f87581eb1c32d596d828
KelvinMutuku <mutukuk553@gmail.com>
2023-03-09 10:57:16 +0300

    103-fast_alphabet.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 0_add.py
+# Kelvin Mutuku <mutukuk553@gmail.com>
+
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
