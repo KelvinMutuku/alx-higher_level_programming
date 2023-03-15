041ef6c61cfbddfabc2ac3d61c371da9a87d6952
KelvinMutuku <mutukuk553@gmail.com>
2023-03-15 19:11:04 +0300

    103-python.c

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 7-update_dictionary.py
+# Kelvin Mutuku <mutukuk553@gmail.com>
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
