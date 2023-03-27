599d1122aae5d5b915b73f40f64663fd3899115f
KELVIN_MUTUKU <118681525+KelvinMutuku@users.noreply.github.com>
2023-03-27 13:21:27 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+# 0-safe_print_list.py
+# kelvinMutuku 
+
+
 def safe_print_list(my_list=[], x=0):
     """Print x elememts of a list.
+
     Args:
         my_list (list): The list to print elements from.
         x (int): The number of elements of my_list to print.
+
     Returns:
         The number of elements printed.
     """
@@ -16,4 +22,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print("")
     return (ret)
-
