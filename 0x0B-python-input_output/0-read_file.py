d8ee0a21d94e793e5c17e95f3077bed4b0385976
KELVIN_MUTUKU <118681525+KelvinMutuku@users.noreply.github.com>
2023-04-12 19:32:52 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-# 0-read_file.py
-# KelvinMutuku
-"""Defines a text file-reading function."""
+"""
+Contains the read_file function
+"""
 
 
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
-    with open(filename, encoding="utf-8") as f:
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
