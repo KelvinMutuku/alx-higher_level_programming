0a93fb78d194c604faac0f3c28e3e0a70e24ea83
KelvinMutuku <mutukuk553@gmail.com>
2023-06-16 12:06:11 +0300

    102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.sql
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.sql
@@ -1,4 +1,2 @@
 -- Insert states
 INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
-
-guillaume@ubuntu:~/0x0F$ cat 7-model_state_fetch_all.sql | mysql -uroot -p hbtn_0e_6_usa
