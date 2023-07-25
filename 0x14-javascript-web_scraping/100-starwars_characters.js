e981643a7dd8abfaf8122188709ea6f0510ce2fa
KELVIN_MUTUKU <118681525+KelvinMutuku@users.noreply.github.com>
2023-07-25 12:58:36 +0300

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,21 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+    return;
   }
+  const characters = JSON.parse(body).characters;
+  characters.forEach((char) => {
+    request(char, (err, res, body) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(JSON.parse(body).name);
+      }
+    });
+  });
 });
